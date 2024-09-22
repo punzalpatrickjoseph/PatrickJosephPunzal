3e68d6626d9f919343695bcec002fef97f711b57
punzalpatrickjoseph <punzalpatrickjoseph17@gmail.com>
2024-09-22 14:57:51 +0800

    lq1_PUNZAL.js

---
+++ lq1_PUNZAL.js
@@ -0,0 +1,22 @@
+var persona = "Name: "Maria Delos Santos Las Marias";
+var age = "Age: 25";
+var address = "Address: Upper Session Road, Baguio City, Benguet";
+
+var persona2 = "Name: DELA CRUZ, Juan Gamoso";
+var age = "Age: 28";
+var address = "Address: San Nicolas, Candon City, Ilocos Sur";
+var per1 = persona1 + ", " + age + ", " + address;
+var per2 = prsona2 + ", " + age + ", " + address;
+
+var personaLength = persona.length;
+var persona2Length = persona.length;
+var addressLength = address.length;
+var addressLength = address.length;
+
+console.log(per1);
+console.log(per2);
+console.log(per1.length + per2.length);
+console.log(25 + 28);
+console.log(persona.length);
+console.log(personaLength * persona2Length);
+console.log(addressLength ** addressLength);
